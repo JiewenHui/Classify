82cd49eb297ca665f8523ceb7d3d07822f60a0a0
Benyou Wang <810553967@qq.com>
2020-03-25 15:26:18 +0800

    add one more version abs

--- model_cnn/abs_fasttext.py
+++ model_cnn/abs_fasttext.py
@@ -15,7 +15,7 @@ from keras import backend as K
 import math
 from model_cnn.fasttext import fasttext
 
-class square_fasttext(fasttext):
+class abs_fasttext(fasttext):
     def __init__(
       self, max_input_left,embeddings,vocab_size,embedding_size,batch_size,filter_sizes,
       num_filters,dataset,l2_reg_lambda = 0.0, is_Embedding_Needed = False,trainable = True,extend_feature_dim = 10):
@@ -24,7 +24,8 @@ class square_fasttext(fasttext):
       num_filters,dataset,l2_reg_lambda, is_Embedding_Needed ,trainable ,extend_feature_dim )
     def get_representation(self,embedded):
         hidden = tf.reduce_sum(embedded,-2)
-        hidden = tf.square(hidden)
+        # hidden = tf.abs(hidden)
+        hidden = tf.abs(hidden)
         return hidden 
 
     
